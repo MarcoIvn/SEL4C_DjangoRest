c0ceee118c1cb85c3686845d493082d5a9f3a39b
Marco Iván <morkiglogxx@gmail.com>
2023-10-03 17:07:59 -0600

    Admin actions added in dashboard

--- SEL4C_DjangoRest/SEL4C_Django/sel4c/views.py
+++ SEL4C_DjangoRest/SEL4C_Django/sel4c/views.py
@@ -6,65 +6,51 @@ from rest_framework.response import Response
 import SEL4C_Django.sel4c.models as models
 import SEL4C_Django.sel4c.serializers as serializers
 from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.views import View
+from .decorators import *
+from django.utils.decorators import method_decorator
 from . import forms
 import json
 
 
-""" class HomeView(View):
-    def get(self, request):
-        users = models.Administrator.objects.all()
-        entrepreneurs = models.Entrepreneur.objects.all()
-        qs = set()
-        qs.union(users, entrepreneurs)
-        activities = models.Activity.objects.all()
-
-        # activity_labels = [f"Actividad {activity.activity_num}" for activity in activities]
-        activity_labels = []
-        activity_deliveries = []
-        for activity in activities:
-            activity_labels.append(f"Actividad {activity.activity_num}")
-            activity_deliveries.append(f"{activity.deliveries}")
-        
-        context = {
-            'entrepreneurs': entrepreneurs,
-            'activity_labels': json.dumps(activity_labels),
-            'activity_deliveries': json.dumps(activity_deliveries)
-        }
-        print(context)
-
-        if request.user.is_authenticated:
-            return render(request, "sel4c/index.html", context)
-        else:
-            messages.success(request, ("Necesita Iniciar Sesión"))
-            return redirect('login')
- """
- 
 class HomeView(View):
+    @method_decorator(staff_required)
     def get(self, request):
-        
         entrepreneurs = models.Entrepreneur.objects.all()
-
         activities = models.Activity.objects.all()
-
-        # Create the context
         context = {
             'entrepreneurs': entrepreneurs,
             'activity_labels': [f"Actividad {activity.activity_num}" for activity in activities],
             'activity_deliveries': [activity.deliveries for activity in activities],
         }
         print(context)
-        if request.user.is_authenticated and request.user.is_staff:
-            return render(request, "sel4c/index.html", context)
-        else:
-            messages.success(request, ("Necesita Iniciar Sesión"))
-            return redirect('login')
+        return render(request, "sel4c/index.html", context)
+
+
+class AdministratorsView(View):
+    @method_decorator(superuser_required)
+    def get(self, request):
+        administrators = models.Administrator.objects.all()
+        context = { 'administrators': administrators}
+        return render(request, "sel4c/user/index.html", context)
+    
+  
+class AdministratorView(View):
+    @method_decorator(superuser_required)
+    def get(self, request, id):
+        administrator = models.Administrator.objects.filter(id = id)
+        context = { 'administrator': administrator}
+        print(context)
+        return render(request, "sel4c/user/show.html", context)
 
 
 
 class EntrepreneurView(View):
+    @method_decorator(staff_required)
     def get(self, request, id):
         # Use Subquery and OuterRef to perform a LEFT JOIN-like operation
         entrepreneur = models.Entrepreneur.objects.filter(id = id)
@@ -187,43 +173,47 @@ class AnswerViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
 
+
 class registerAdministrator(View):
+    @method_decorator(superuser_required)
     def get(self,request):
-      if request.user.is_authenticated and request.user.is_superuser:
         form = forms.RegisterAdministratorForm()
         return render(request, 'sel4c/user/new.html', {"form": form})
-      else: 
-        return redirect("/home")
+    @method_decorator(superuser_required)
     def post(self, request):
-        if request.user.is_authenticated and request.user.is_superuser:
-            form = forms.RegisterAdministratorForm(request.POST)
-            if form.is_valid():
-              try:
-                form.save()
-              except:
-                messages.error(request, ("Error al crear usuario"))
-                return redirect('register')
-              else:
-                #username = form.cleaned_data["username"]
-                #password = form.cleaned_data["password1"]
-                messages.success(request, ("Usuario creado exitosamente"))
-                return redirect('home')
-            else:
-              return render(request, 'sel4c/user/new.html', {"form": form})
-        else: 
-          return redirect("/home")
-      
-# UPDATE User
-def change_user(request):
-    if request.method == 'POST':
-        form = forms.ChangeUserForm(request.POST, instance=request.user)
+        form = forms.RegisterAdministratorForm(request.POST)
         if form.is_valid():
+          try:
             form.save()
-            messages.success(request, '¡Su perfil se ha actualizado!')
+          except:
+            messages.error(request, ("Error al crear usuario"))
+            return redirect('register')
+          else:
+            #username = form.cleaned_data["username"]
+            #password = form.cleaned_data["password1"]
+            messages.success(request, ("Usuario creado exitosamente"))
             return redirect('home')
         else:
-            messages.error(request, 'Please correct the error below.')
+          return render(request, 'sel4c/user/new.html', {"form": form})
+      
+# UPDATE User
+@superuser_required
+def editAdministrator(request,id):
+    administrator = get_object_or_404(models.Administrator, id=id)
+    if request.method == 'POST':
+        form = forms.RegisterAdministratorForm(request.POST, instance=administrator)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Administrador actualizado exitosamente.')
+            return redirect('home')  
     else:
-        form = forms.RegisterAdministratorForm()
-        return render(request, 'sel4c/register_user.html', {"form": form})
+        form = forms.RegisterAdministratorForm(instance=administrator)
+        return render(request, 'sel4c/user/edit.html', {"form": form, "administrator": administrator})
+
+@superuser_required
+def deleteAdministrator(request, id):
+    administrator = get_object_or_404(models.Administrator, id=id)
+    administrator.delete()
+    messages.success(request, 'Administrador eliminado exitosamente.')
+    return redirect('administrators')  
 
